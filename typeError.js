3f72593c79b7549b9b813a7822e8482989563f9c
antik1801 <antik.edu@gmail.com>
2023-03-06 15:03:09 +0600

    at last commit

--- typeError.js
+++ typeError.js
@@ -6,3 +6,15 @@ let address = {
     postCode: '1296',
 }
 console.log(address?.home?.active);
+
+const sum = (a,b) =>{
+    return a+b;
+}
+// console.log(sum2(15,20));
+
+const doSomething = (a,b) =>{
+    const result = a + b;
+}
+console.log(doSomething(15,5));
+console.log(result);
+
