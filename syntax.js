e858ae30f048e177aa80f4617b5fc28dcfd16a43
antik1801 <antik.edu@gmail.com>
2023-03-06 11:05:23 +0600

    first commit

---
+++ syntax.js
@@ -0,0 +1,26 @@
+function isBigger(number) {
+    if (number > 5) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+const result = isBigger(11);
+console.log(result)
+
+
+const sum = (a,b) => a+b;
+const res = sum(3,5);
+console.log(res);
+
+const musk = {
+    name: 'Elon',
+    age: 25,
+    job: 'CEO of Space-X',
+
+}
+
+console.log(musk);
+
+
